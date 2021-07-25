064c39212e488053ec87c2cd8ccadf15fa9a5380
Austin Chabaud <austinc1023@yahoo.com>
2021-07-24 20:45:01 -0500

    changed F to C

--- src/App.js
+++ src/App.js
@@ -1,67 +1,106 @@
 import React, { useState } from 'react';
 const api = {
-  key: "7063f267cff128b1bac50c51ac1621a4",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+	key: '7063f267cff128b1bac50c51ac1621a4',
+	base: 'https://api.openweathermap.org/data/2.5/',
+};
 function App() {
-  const [query, setQuery] = useState('')
-  const [weather, setWeather] = useState('')
+	const [query, setQuery] = useState('');
+	const [weather, setWeather] = useState('');
 
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result)
-          setQuery('')
-          console.log(weather)
-        })
-    }
-  }
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	const search = (evt) => {
+		if (evt.key === 'Enter') {
+			fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+				.then((res) => res.json())
+				.then((result) => {
+					setWeather(result);
+					setQuery('');
+					console.log(weather);
+				});
+		}
+	};
+	const dateBuilder = (d) => {
+		let months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		];
+		let days = [
+			'Sunday',
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+		];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+		let day = days[d.getDay()];
+		let date = d.getDate();
+		let month = months[d.getMonth()];
+		let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+		return `${day} ${date} ${month} ${year}`;
+	};
+	return (
+		<div
+			className={
+				typeof weather.main != 'undefined'
+					? weather.main.temp > 16
+						? 'App warm'
+						: 'App'
+					: 'App'
+			}
+		>
+			<main>
+				<div className='search-box'>
+					<input
+						type='text'
+						className='search-bar'
+						placeholder='Search any city or country!'
+						onChange={(e) => setQuery(e.target.value)}
+						value={query}
+						onKeyPress={search}
+					/>
+				</div>
+				{typeof weather.main != 'undefined' ? (
+					<div>
+						<div className='location-box'>
+							<div className='location'>
+								{weather.name}, {weather.sys.country}
+							</div>
+							<div className='date'>{dateBuilder(new Date())}</div>
+						</div>
+						<div className='weather-box'>
+							<div className='temp'>{Math.round(weather.main.temp)}°C</div>
+							<div className='weather'>{weather.weather[0].main}</div>
+						</div>
+					</div>
+				) : (
+					''
+				)}
 
-  }
-  return (
-    <div className={
-      (typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'App warm' : 'App') : 'App'}>
-      <main>
-        <div className="search-box">
-          <input type="text"
-            className="search-bar"
-            placeholder="Search any city or country!"
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search} />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-          <div>
-            <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">
-                {Math.round(weather.main.temp * 1.8 + 32)}°F
-          </div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
-        ) : ('')}
-      
-      <h2>Created by <a href="https://www.austinchabaud.com" target="_blank" rel="noopener noreferrer">Austin Chabaud</a></h2>
-      </main>
-    </div>
-  );
+				<h2>
+					Created by{' '}
+					<a
+						href='https://www.austinchabaud.com'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Austin Chabaud
+					</a>
+				</h2>
+			</main>
+		</div>
+	);
 }
-
 
 export default App;
